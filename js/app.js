a76a34e78fde470362900940f3eaaac638e9bb1b
Rafael Simas <rafael.simas@al.resilia.com.br>
2024-03-18 10:34:31 -0300

    Cartão virtual V1

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const openPixKey = () => {
+    const openCloseCard  = document.querySelector('.open-close-card ')
+    openCloseCard.classList.remove('escondido')
+}
+
+
+
+const btnCloseCardPix = () => {
+    const openCloseCard  = document.querySelector('.open-close-card ')
+    openCloseCard.classList.add('escondido')
+}
+
+
+
+
+
